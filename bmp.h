f372c85a3b82625f75c8ffb518a3ab8e1ad93002
Diana <diana.gakova2006@gmail.com>
2026-01-18 18:04:59 +0300

    Update bmp.h

--- bmp.h
+++ bmp.h
@@ -1,24 +1,22 @@
-
+// bmp.h
 #ifndef BMP_H
 #define BMP_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-// Один пиксель — красный, зеленый, синий
 typedef struct {
     unsigned char r, g, b;
 } Pixel;
 
-// Изображение — ширина, высота, двумерный массив пикселей
 typedef struct {
-    int w, h;           // ширина и высота
-    Pixel** pix;        // массив пикселей: pix[y][x]
+    int w, h;
+    Pixel** pix;
 } Img;
 
-// Функции для работы с BMP
 Img* load_bmp(const char* fname);
 int save_bmp(const char* fname, Img* img);
 void free_img(Img* img);
 
 #endif
+
